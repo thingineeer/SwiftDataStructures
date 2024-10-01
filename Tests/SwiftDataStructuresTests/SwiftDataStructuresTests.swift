5fa5b7cac79223c0dc278f2369099820fb4e4914
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-10-01 18:01:05 +0900

    [Test] - Heap TestCode 추가

--- Tests/SwiftDataStructuresTests/SwiftDataStructuresTests.swift
+++ Tests/SwiftDataStructuresTests/SwiftDataStructuresTests.swift
@@ -2,11 +2,32 @@ import XCTest
 @testable import SwiftDataStructures
 
 final class SwiftDataStructuresTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    var heap: Heap<Int>!
+    
+    override func setUp() {
+        super.setUp()
+        self.heap = Heap<Int>(sortFunction: >)
+    }
+    
+    override func tearDown() {
+        heap = nil
+        super.tearDown()
+    }
+    
+    func test_SwimUp() {
+        for i in [1, 9, 8, 10] {
+            heap.insert(i)
+        }
+        
+        XCTAssertEqual(heap.description, "[10, 9, 8, 1]")
+    }
+    
+    func test_삽입_삭제() {
+        heap.insert(10)
+        heap.insert(9)
+        heap.insert(8)
+        heap.insert(1)
+        XCTAssertEqual(heap.remove(), 10)
     }
 }
